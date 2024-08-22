854348f49f244a30afbbe2bb9d10fa89a82b7ff7
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-08-22 13:39:23 +0300

    22/08/24

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,18 +2,27 @@ import { useState } from "react";
 import contactDate from "../../contact.json";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
+import ContactForm from "../ContactForm/ContactForm";
 
 export default function App() {
   const [contacts, setContacts] = useState(contactDate);
+  const [filter, setFilter] = useState("");
 
-  // const addContacts ()=>{}
+  const addContacts = newContacts => {
+    setContacts(prevContacts => {
+      return [...prevContacts, newContacts];
+    });
+  };
 
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <div>
       <h1>Phonebook</h1>
-      {/* <ContactForm /> */}
-      <SearchBox />
-      <ContactList contacts={contacts} />
+      <ContactForm onAdd={addContacts} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContacts} />
     </div>
   );
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,14 @@
 import css from "./SearchBox.module.css";
 
-export default function SearchBox() {
+export default function SearchBox({ value, onFilter }) {
   return (
     <div>
       <p>Find contact by name</p>
-      <input type="text" />
+      <input
+        type="text"
+        value={value}
+        onChange={evt => onFilter(evt.target.value)}
+      />
     </div>
   );
 }
